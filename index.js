a6742ba6885ed3d5e8620a98891d7dd36ed6c299
Florian Blaschke <florianblaschke@web.de>
2023-05-08 11:36:45 +0200

    fixed

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
-        const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
-        const arrow = card.querySelector('[data-js="arrow"]')
-          button.addEventListener('click',() => {
-          link.classList.toggle("hide")
-          arrow.classList.toggle("rotate")
-          
-    })
-})
-}
+const cards = document.querySelectorAll('[data-js="card"]');
+
+cards.forEach(card => {
+  const button = card.querySelector('[data-js="button"]');
+  const link = card.querySelector('[data-js="link"]');
+  const arrow = card.querySelector('[data-js="arrow"]');
+  card.addEventListener('click', () => {
+    link.classList.toggle('hidden');
+    arrow.classList.toggle('rotate');
+  });
+});
+
+// cards.forEach(card => {
+//   const button = card.querySelector('[data-js="button"]');
+//   const link = card.querySelector('[data-js="link"]');
+//   const arrow = card.querySelector('[data-js="arrow"]');
+//   button.addEventListener('click', () => {
+//     link.classList.toggle('hide');
+//     arrow.classList.toggle('rotate');
+//   });
+// });
